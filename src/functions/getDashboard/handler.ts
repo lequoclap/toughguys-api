a1627f7cee510e2c75b6e51e4e94e339351562b7
Le Quoc Lap <quoclap.hedspi@gmail.com>
2022-12-20 01:25:53 +0900

    removed unnecessary stufts

--- src/functions/getDashboard/handler.ts
+++ src/functions/getDashboard/handler.ts
@@ -7,20 +7,6 @@ import schema from './schema';
 
 const getDashboard: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
 
-
-  // const response = await createOrUpdateStravaData('74721176', {
-  //   athlete: {
-  //     id: '74721176',
-  //     name: 'Le Lap',
-  //     imgURL: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/74721176/26004307/1/medium.jpg',
-  //     accessToken: '2996e2324ae5d1e5d6677319570b6f463d0b4cf7',
-  //     refreshToken: '97f5fb168437a0474f2ce777d3f0680f39043de6',
-  //     activities: []
-  //   }
-  // });
-
-
-
   // fetch all id from id
 
   // fetch all activities
