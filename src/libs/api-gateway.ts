b1234394f7e9ae3f9d7b9c5953e06b26aeb8fbf9
Le Quoc Lap <quoclap.hedspi@gmail.com>
2022-12-25 00:26:49 +0900

    introduced header to avoid CORs issue

--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -5,14 +5,40 @@ type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { b
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
 export const formatJSONResponse = (response: Record<string, unknown>) => {
+
+  // idea about multi whitelist domain
+  // const corsWhitelist = [
+  //   'http://localhost:5000',
+  //   'http://laplq.com',
+  //   'https://domain3.example'
+  // ];
+  // if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+  //   res.header('Access-Control-Allow-Origin', req.headers.origin);
+  //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // }
+
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    // 'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS,
+    'Access-Control-Allow-Headers': '*'
+  };
+
   return {
+    headers,
     statusCode: 200,
     body: JSON.stringify(response)
   }
 }
 
 export const formatErrorResponse = (statusCode: number, message: string) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    // 'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS,
+    'Access-Control-Allow-Headers': '*'
+  };
   return {
+    headers,
     statusCode,
     body: JSON.stringify(message)
   }
